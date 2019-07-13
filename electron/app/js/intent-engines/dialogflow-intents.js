6f05a260b863bbd840e5f1aace458a99d81d696c
Adrian <a.schneider@asfs.de>
2019-07-14 00:15:43 +0200

    Added new skill structure and handling so that new skills can be implemented in a more modular way; also Peeqo looks up the responseText or intent name by default
    Added new skill structure and lookup of responseText or intent name

--- electron/app/js/intent-engines/dialogflow-intents.js
+++ electron/app/js/intent-engines/dialogflow-intents.js
@@ -3,6 +3,9 @@ const weather = require('js/skills/weather')
 const Timer = require('js/skills/timer')
 const event = require('js/events/events')
 const responses = require('js/responses/responses')
+const skill = require('js/skills/skill')
+const fs = require('fs')
+const path = require('path')
 
 function parseIntent(cmd){
 
@@ -39,7 +42,53 @@ function parseIntent(cmd){
 			actions.setAnswer(responses.bye, {type: 'local'})
 			break
 		default:
-			actions.setAnswer(responses.confused, {type:'local'})
+			/** Check if a skill with the name of this intent exists in js/skills **/
+                        let skillPath = path.join(process.cwd(),'app','js','skills', cmd.intent + '.js')
+                        if (fs.existsSync(skillPath)) {
+                                /** Include skill **/
+                                let skill = require('js/skills/'+cmd.intent)
+
+                                /** Check if skill is inherited from Skill class **/
+                                if (Object.getPrototypeOf(skill.constructor).name == "Skill") {
+                                        console.log(`Running skill ${cmd.intent} from js/skills/${cmd.intent}.js`)
+
+                                        /** Initialize skill **/
+                                        skill.init()
+
+                                        /** Set responseText from dialogflow **/
+                                        if (cmd.responseText) {
+                                                skill.setResponseText(cmd.responseText)
+                                        }
+
+                                        /** Set dialogflow parameters **/
+                                        for (var paramName in cmd.params) {
+                                                if (cmd.params[paramName].stringValue) {
+                                                        skill.setParam(paramName, cmd.params[paramName].stringValue)
+                                                }
+                                        }
+
+                                        /** Run skill and stop **/
+                                        skill.run()
+                                        break
+
+                                /** If skill is not inherited from Skill class, log error and go on **/
+                                } else {
+                                        console.log(`${cmd.intent} is not of type Skill (${Object.getPrototypeOf(skill.constructor).name})`)
+                                }
+                        }
+
+                        let responseTerms = []
+                        let responseLed = {anim:'blink', color: 'green'}
+
+                        /** If no skill for this intent exists, check if dialogflow gave us a responseText, 
+			    otherwise use the name of the intent **/
+                        if (cmd.responseText) {
+                                responseTerms = [cmd.responseText]
+                        } else {
+                                responseTerms = [cmd.intent]
+                                responseLed.color = 'orange'
+                        }
+                        actions.setAnswer({queryTerms: responseTerms, led: responseLed}, {type:'remote'})
 			break
 	}
 
